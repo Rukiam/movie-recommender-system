6bf9e2759762bb062468471da1f2476198b11c54
Rukiam <rukiamd67@email.com>
2026-05-22 01:43:43 +0800

    Add SQL schema and create SQLite database

---
+++ sql/sql/schema.sql
@@ -0,0 +1,20 @@
+-- Movie Recommender System
+-- Schema Design
+
+CREATE TABLE IF NOT EXISTS movies ( 
+Prevents errors if you run the script multiple times
+    movieId INTEGER PRIMARY KEY,
+    title   TEXT NOT NULL,
+    genres  TEXT
+);
+
+-- Creates a table called 'movies' ONLY if it doesn't already exist
+-- Prevents errors if you run the script multiple times
+
+CREATE TABLE IF NOT EXISTS ratings (
+    userId    INTEGER,
+    movieId   INTEGER,
+    rating    REAL,
+    timestamp INTEGER,
+    FOREIGN KEY (movieId) REFERENCES movies(movieId)
+);
